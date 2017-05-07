7f167efde318806d63c50c3172458ffe6fd92f55
Or Adar <adaror@gmail.com>
2017-05-07 12:11:50 +0300

    forth commit
    final project - local database

--- complain_module.js
+++ complain_module.js
@@ -3,23 +3,26 @@
  */
 const data = require('./data/data.json');
 
+//Return the JSON data file
 exports.getAllComplains = ()=> {
     return data;
 };
 
+//-------------return complains by type-----------------
 exports.getByType = (type)=>{
     for(let i in data.complains){
-        if (data.complains[i].type == type){
-            return data.complains[i].complains;
+        if (data.complains[i].type == type){       //if the type in JSON file equal to the parameter
+            return data.complains[i].complains;    //return the complains
         }
     }
     return false;
 };
 
+//---------return complain by user name and type of complain--------
 exports.getByUserAndType = function(user,type){
-    var output =`{"result":[`;
+    var output =`{"result":[`;                  //global string that strings the JSON
 
-    let flag = false;
+    let flag = false;                           //flag that mark if the service was found in the JSON file
     for(var i in data.complains){
         if (data.complains[i].type == type){
             flag = true;
@@ -29,18 +32,18 @@ exports.getByUserAndType = function(user,type){
 
     if(flag){
 
-     for (let j in data.complains[i].complains)  {
+     for (let j in data.complains[i].complains)  {              //looking for the user name inside the type section
          if (data.complains[i].complains[j].name == user){
-             output += JSON.stringify(data.complains[i].complains[j])+`,`;
+             output += JSON.stringify(data.complains[i].complains[j])+`,`;      //string the json file
          }
      }
 
-     if (output == `{"result":[`){
+     if (output == `{"result":[`){          //if the parameters was incorrect return false
          return false;
      }
-     output = output.substr(0,output.length-1);
-     output+=`]}`;
-     return JSON.parse(output);
+     output = output.substr(0,output.length-1);     //erase the last ','
+     output+=`]}`;                                  //close the JSON file
+     return JSON.parse(output);                     //convers the string to JSON and send back
     }
 
     return false;
--- index.js
+++ index.js
@@ -10,41 +10,45 @@ const express         = require('express'),
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-
+//-----------Send all JSON by GET method------------
 app.get('/showAllComplains',
     (req,res)=>{
     let allComplains = complainModule.getAllComplains();
-    res.status(200).send(allComplains);
+    res.status(200).json(allComplains);
 });
 
+//----------Send Json of complain by complain type using POST------
 app.post('/getType/',
     (req,res,next)=>{
     let type = req.body.type_name;
-    let parameters = complainModule.getByType(type);
-   if (parameters == false){
+    let parameters = complainModule.getByType(type);//call the module function
+   if (parameters == false){                        //if the module return false then fallback
        next();
    }else{
-   res.status(200).send(parameters);}
+   res.status(200).json(parameters);}
 });
 
-app.get('/getComplainByUserAndType/:user_name,:type_name',
+//-------------Send JSON of complains by using name and type parameters by GET method--------
+app.get('/getComplainByUserAndType/:user_name/:type_name',
     (req,res,next)=>{
-   let user = req.params.user_name;
-   let type = req.params.type_name;
-   let parameters = complainModule.getByUserAndType(user,type);
+   let user = req.params.user_name;                                 //catch the user name parameter
+   let type = req.params.type_name;                                 //catch the type parameter
+   let parameters = complainModule.getByUserAndType(user,type);     //call the function
 
     if (parameters == false){
        next();
     }else{
-    res.send(parameters);
+    res.status(200).json(parameters);
     }
 });
 
+//----------Fallback - return error if one of the parameters was wrong-----------
 app.all('*',
     (req,res)=>{
     res.json({"Error":"No Parameters Found"});
     });
 
+//---------------listening to port-------------------
 app.listen(port,
     ()=>{
     console.log(`listening on port ${port}`);
